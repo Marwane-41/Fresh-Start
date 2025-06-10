f309026816f53e89f387e595f61440df02783a9c
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-06-10 11:14:32 -0600

    Welcome page + fixes in style

---
+++ business-template1/pages/index.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div>  
+
+      <h1 className=" text-3xl font-bold underline text-center"> 
+        Hello world 
+      </h1>
+    </div>
+  );
+}
---
+++ business-template1/pages/welcome.js
@@ -0,0 +1,18 @@
+export default function Welcome() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-[#fff7ec] text-[#4e342e] ">  
+        <div className=" text-center p-8"> 
+        <h1 className="text-4xl font-bold mb-4"> 
+          Welcome to Ola's Bakery 
+        </h1>
+
+        <p className="text-lg mb-6">
+          Deliciously handmade. Fresh every morning.
+        </p>
+
+        </div>
+        
+      </div>
+    );
+  }
+  
