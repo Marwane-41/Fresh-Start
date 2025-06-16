749499a28fe4e67c407ea0adfad0e61c8caa71bf
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-06-16 11:20:27 -0600

    Update Home.js

--- business-template1/pages/components/Home.js
+++ business-template1/pages/components/Home.js
@@ -6,15 +6,8 @@ export default function HomePage(){
           <h1 className="text-5xl md:text-6xl font-edu text-[#5e3023] mb-6">
             Welcome to Olas Bakery
           </h1>
-          <p className="text-lg md:text-xl font-sans text-[#5e3023] max-w-xl mb-8">
-            Homemade pastries, baked fresh every morning with love and tradition.
-          </p>
-          <a
-            href="/menu"
-            className="bg-[#d5a845] text-white px-6 py-3 rounded-xl shadow-md hover:bg-[#c2983c] transition"
-          >
-            Explore Our Menu
-          </a>
+          
+         
         </section>
       );
 }
