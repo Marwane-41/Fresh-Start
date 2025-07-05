88fa5ca35b7c02ac47cb1ea757f5dd42e60e9de8
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-07-05 06:22:54 -0600

    Update Home.js

--- business-template1/pages/components/Home.js
+++ business-template1/pages/components/Home.js
@@ -1,6 +1,5 @@
 export default function HomePage(){
 
-
     return (
         <section className="bg-[#fff7ec] min-h-screen flex flex-col items-center justify-center text-center px-4">
           <h1 className="text-5xl md:text-6xl font-edu text-[#5e3023] mb-6">
